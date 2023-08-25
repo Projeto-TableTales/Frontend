282f9a1f35856e2187e542bdc5c2400997b47395
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-08-25 15:56:35 -0300

    changed style login

--- src/components/loginForm/styled.js
+++ src/components/loginForm/styled.js
@@ -1,67 +1,59 @@
 import styled from "styled-components";
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
 
 export const Container = styled.div`
-    height: 80vh;
-    width: 612px;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 0 30px 30px 0;
-    background-color: #D9D9D9;
+  height: 80vh;
+  width: 612px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0 30px 30px 0;
+  background-color: #d9d9d9;
 `;
 export const Title = styled.h1`
-    color: #D2C5C5;
-    background-color: #643A6F;
-    width: 184px;
-    border-radius: 11.6px;
-    text-align: center;
+  color: #d2c5c5;
+  background-color: #643a6f;
+  width: 184px;
+  border-radius: 24px;
+  text-align: center;
+  font-size: 24px;
+  padding: 10px;
 `;
 export const Form = styled.form`
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    gap:1.5rem;
-`;
-export const Box = styled.div`
-    width : 439px;
-    display:flex;
-    flex-direction: column;
-    padding: 1rem 3rem 2rem 3rem;
-    background-color:#85608E;
-    border-radius: 20px;
-    color: #290532 !important;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
 `;
 
-export const Input = styled.input` 
-    padding: 8px;
-    outline: none;
-    border: none;
-    background-color: #D9D9D9;
-    border-radius: 11.6px;
-    margin-top: 2rem;
-    margin-bottom: 1rem;
+export const Input = styled.input`
+  background-color: #eee;
+  border: none;
+  padding: 14px 15px;
+  width: 270px;
 `;
 
 export const CustomRouterLink = styled(RouterLink)`
-    color: #290532 !important;
-    align-self: flex-end;
+  color: #290532 !important;
+  align-self: flex-end;
 `;
 
 export const Button = styled.button`
-    color: #D2C5C5;
-    background-color: #643A6F;
-    width: 141px;
-    padding: 5px;
-    font-size: 17px;
-    font-weight: bold;
-    border-radius: 11.6px;
-    cursor: pointer;
-    border: none;
+  border-radius: 20px;
+  border: 1px solid #643a6f;
+  background-color: #643a6f;
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
+  &:active {
+    transform: scale(0.95);
+  }
+  &:focus {
     outline: none;
-    &:hover{
-        background-color: #2C0445;
-        box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-    }
+  }
 `;
