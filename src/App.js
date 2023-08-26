72b6044694022e53cc6f168a1750490c60cf1fa4
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-08-26 13:58:32 -0300

    add route perfil

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
+import Perfil from './pages/perfil/perfil';
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes> {/* Alterado de <Switch> */}
       <Route path="/" element={<Login />} /> {/* Alterado de <Route exact path="/login" component={Login} /> */}
       <Route path="/register" element={<Register />} /> {/* Alterado de <Route path="/register" component={Register} /> */}
+      <Route path="/perfil" element ={<Perfil />}/>
     </Routes>
   </BrowserRouter>
   );
--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,20 +1,23 @@
-import { Button, Container, Form, Input, Title} from "./styled";
-import { Link } from 'react-router-dom';
+import { Button, Container, Form, Input, Title } from "./styled";
+import { Link } from "react-router-dom";
 const LoginForm = () => {
-    return(
-        <Container>
-            <Title> SIGN IN</Title>
-            <Form>
+  return (
+    <Container>
+      <Title> SIGN IN</Title>
+      <Form>
+        <Input type="email" placeholder="E-mail" />
+        <Input type="password" placeholder="Password" />
+        <Link href="">Forgot your password?</Link>
+        <Link to="/perfil">
+          <Button>ENTER</Button>
+        </Link>
+      </Form>
 
-                    <Input type="email" placeholder="E-mail"/>
-                    <Input type="password" placeholder="Password"/>
-                    <Link href="">Forgot your password?</Link>
-                <Button>ENTER</Button>
-            </Form>
-            
-            <p>New here? <Link to="/register">Create an account</Link> </p>
-        </Container>
-    )
-}
+      <p>
+        New here? <Link to="/register">Create an account</Link>{" "}
+      </p>
+    </Container>
+  );
+};
 
 export default LoginForm;
