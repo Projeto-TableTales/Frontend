15036fe81fea185a58639f085fb580317d4858b0
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-08-26 14:55:14 -0300

    add AppBar in Perfil

---
+++ src/components/appbar/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { AppBar, Toolbar } from '@mui/material';
+
+// Estilos do AppBar
+export const CustomAppBar = styled(AppBar)`
+
+`;
+
+export const Bar = styled(Toolbar)`
+background-color: #2C0445; 
+`;
+
+// Estilos da logo
+export const Logo = styled.img`
+margin-top: 10px;
+  height: 70px;
+`;
---
+++ src/pages/perfil/perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '../../components/appbar/appBar'; // Importe o componente da AppBar que você criou
+
+function Perfil() {
+  return (
+    <div>
+      <AppBar /> 
+    </div>
+  );
+}
+
+export default Perfil;
