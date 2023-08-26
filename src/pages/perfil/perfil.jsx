54e01ac5bb76b9e04d0fc698d4d6a9be2323f392
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-08-26 17:40:22 -0300

    add area description

--- src/pages/perfil/perfil.jsx
+++ src/pages/perfil/perfil.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppBar from "../../components/appbar/appBar"; // Importe o componente da AppBar que você criou
 import AreaPerfil from "../../components/areaPerfil/areaPerfil";
+import {Description} from "./styled";
 
 function Perfil() {
   return (
     <div>
       <AppBar />
      <AreaPerfil/>
+     <Description></Description>
     </div>
   );
 }
--- src/pages/perfil/styled.js
+++ src/pages/perfil/styled.js
@@ -1,2 +1,12 @@
 import styled from "styled-components";
 
+export const Description = styled.div`
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #D6BCE9;
+  margin: 50px 50px;
+ `;
+
+ 
