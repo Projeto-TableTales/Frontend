455fb23816dedc19187a3e0ed4b4c688e085f6ea
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-09-10 14:49:49 -0300

    add Error Handling in RegisterPage

--- src/components/registerForm/registerForm.jsx
+++ src/components/registerForm/registerForm.jsx
@@ -1,62 +1,116 @@
-import { useState } from 'react';
-import {Button, Container, Form, Input,EmailInput, Link,Line, Title} from "./styled";
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  Input,
+  EmailInput,
+  Link,
+  Line,
+  Title,
+  ErrorMessage,
+} from "./styled";
 import axios from "axios";
-import {Navigate} from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
 const RegisterForm = () => {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [role] = useState('USER');
-  const [genero, setGenero] = useState('');
-  const [pais, setPais] = useState('');
-  const [dataNascimento, setDataNascimento] = useState('');
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [role] = useState("USER");
+  const [genero, setGenero] = useState("");
+  const [pais, setPais] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+  const [errMsg, setErrMsg] = useState("");
   const [navigate, setNavigate] = useState(false);
 
-  const submit = async e => {
-      e.preventDefault();
-      try {
-        const response =   await axios.post('http://localhost:8080/rpgwiki/register', {
-          nome, pais, email, senha, genero, role, dataNascimento
-      });
-  // Extrair o token da resposta
-  const token = response.data.token;
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/rpgwiki/register",
+        {
+          nome,
+          pais,
+          email,
+          senha,
+          genero,
+          role,
+          dataNascimento,
+        }
+      );
+      // Extrair o token da resposta
+      const token = response.data.token;
 
-  // Armazenar o token no localStorage
-  localStorage.setItem('token', token);
+      // Armazenar o token no localStorage
+      localStorage.setItem("token", token);
       setNavigate(true);
-      
+
       if (navigate) {
-        return <Navigate to="/home"/>;
+        return <Navigate to="/home" />;
       }
     } catch (error) {
-      console.error('Erro de registro:', error);
+      console.error("Erro de registro:", error);
+      if (error.response) {
+        setErrMsg("Ocorreu um erro ao fazer o registro. Tente novamente.");
+      }
     }
-  }
-
-
+  };
   return (
     <Container>
       <Title> SIGN UP</Title>
       <Form onSubmit={submit}>
         <Line>
-          <Input type="text" placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
-          <Input type="text" placeholder="Genero" value={genero} onChange={e => setGenero(e.target.value)} />
+          <Input
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="Genero"
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+          />
         </Line>
         <Line>
-          <Input type="senha" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
-          <Input type="senha" placeholder="ConfirmSenha"/* onChange={e => setConfirmSenha(e.target.value)}*/ />
+          <Input
+            type="senha"
+            placeholder="Senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <Input
+            type="senha"
+            placeholder="ConfirmSenha" /* onChange={e => setConfirmSenha(e.target.value)}*/
+          />
         </Line>
-        <EmailInput type="email" placeholder="E-mail"  onChange={e => setEmail(e.target.value)} />
+        <EmailInput
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <Line>
-          <Input type="text" placeholder="Pais"  onChange={e => setPais(e.target.value)} />
-          <Input type="text" placeholder="DataNascimento" onChange={e => setDataNascimento(e.target.value)} />
+          <Input
+            type="text"
+            placeholder="Pais"
+            onChange={(e) => setPais(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="DataNascimento"
+            onChange={(e) => setDataNascimento(e.target.value)}
+          />
         </Line>
+        {errMsg ? <ErrorMessage>{errMsg}</ErrorMessage> : null}
         <Button type="submit">REGISTER</Button>
       </Form>
-      <p>Already have an account?<Link to="/login">Login</Link> </p>
+      <p>
+        Already have an account?<Link to="/login">Login</Link>{" "}
+      </p>
     </Container>
   );
-}
+};
 
 export default RegisterForm;
--- src/components/registerForm/styled.js
+++ src/components/registerForm/styled.js
@@ -1,70 +1,73 @@
 import styled from "styled-components";
 import { Link as RouterLink } from "react-router-dom";
 export const Container = styled.div`
-    height: 80vh;
-    width: 612px;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 0 30px 30px 0;
-    background-color: #D9D9D9;
+  height: 80vh;
+  width: 612px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0 30px 30px 0;
+  background-color: #d9d9d9;
 `;
 export const Title = styled.h1`
-    color: #D2C5C5;
-    background-color: #643A6F;
-    width: 184px;
-    border-radius: 11.6px;
-    text-align:center;
+  color: #d2c5c5;
+  background-color: #643a6f;
+  width: 184px;
+  border-radius: 11.6px;
+  text-align: center;
 `;
 export const Form = styled.form`
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    gap:1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
 `;
 export const Line = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap:1.5rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1.5rem;
 `;
 
-export const Input = styled.input` 
-    border: none;
-    padding: 10px 15px;
-    width : 240px;
-    outline: none;
-    border: none;
-    background-color: #FFFFFF;
-    border-radius: 2.5px;
-    margin-top: 0.2rem;
-    margin-bottom: 0.2rem;
-    
+export const ErrorMessage = styled.form`
+  color: red;
+`;
+
+export const Input = styled.input`
+  border: none;
+  padding: 10px 15px;
+  width: 240px;
+  outline: none;
+  border: none;
+  background-color: #ffffff;
+  border-radius: 2.5px;
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
 `;
 export const EmailInput = styled(Input)`
-    width : 540px;
-    padding:11px
+  width: 540px;
+  padding: 11px;
 `;
 
 export const Link = styled(RouterLink)`
-    color: #290532 !important;
-    align-self: flex-end;
+  color: #290532 !important;
+  align-self: flex-end;
 `;
 
 export const Button = styled.button`
-    color: #D2C5C5;
-    background-color: #643A6F;
-    width: 141px;
-    padding: 10px;
-    font-size: 17px;
-    font-weight: bold;
-    border-radius: 11.6px;
-    cursor: pointer;
-    border: none;
-    outline: none;
-    &:hover{
-        background-color: #2C0445;
-        box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-    }
+  color: #d2c5c5;
+  background-color: #643a6f;
+  width: 141px;
+  padding: 10px;
+  font-size: 17px;
+  font-weight: bold;
+  border-radius: 11.6px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  &:hover {
+    background-color: #2c0445;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  }
 `;
