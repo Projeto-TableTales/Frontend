b7f9e3b3051de9c853f712c3f9f9859a573b9311
walmorfelicio <walmor.felicio@dcx.ufpb.br>
2023-09-10 23:27:49 -0300

    Commit 1(branch feacture-perfil): Tela de Perfil de Usuário

---
+++ src/pages/tela_perfil_usuario/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import App_2 from './App_2.jsx'
+//import './index.css'
+import './index_2.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  /*
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  */
+  <React.StrictMode>
+    <App_2 />
+  </React.StrictMode>
+)
