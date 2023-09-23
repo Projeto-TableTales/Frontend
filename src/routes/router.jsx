5c27db034bbfae6f6013f6b3528ac0f418cb0e2c
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-09-23 15:43:51 -0300

    feat persistente route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,23 +1,37 @@
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
-import Login from "../pages/login/login"
-import Register from "../pages/register/register"
-import ForgotPassword from "../pages/forgotPassword/forgotPassword"
-import Home from "../pages/home/home"
-import Apresentacao from "../pages/apresentacao/apresentacao"
-import Perfil from "../pages/perfil/Perfil_Usuario"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "../pages/login/login";
+import Register from "../pages/register/register";
+import ForgotPassword from "../pages/forgotPassword/forgotPassword";
+import Home from "../pages/home/home";
+import Apresentacao from "../pages/apresentacao/apresentacao";
+import Perfil from "../pages/perfil/Perfil_Usuario";
 
-export const AppRouter = () =>{
-    return (
-        <Router>
-            <Routes>
-                <Route path = "/login" element ={<Login/>} />
-                <Route path = "/register" element ={<Register/>} />
-                <Route path = "/forgotPassword" element ={<ForgotPassword/>} />
-                <Route path = "/home" element ={<Home/>} />
-                <Route path = "/" exact element ={<Apresentacao/>} />
-                <Route path = "/perfil" exact element ={<Perfil/>} />
-            </Routes>
-        </Router>
-    )
-
-}
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route
+          path="/home"
+          element={
+            localStorage.getItem("token") ? <Home /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/" exact element={<Apresentacao />} />
+        <Route
+          path="/perfil"
+          element={
+            localStorage.getItem("token") ? <Home /> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
