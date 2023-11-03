ef80b106b4e206c5ee2448d6c643f47065aae020
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-11-03 13:22:50 -0300

    Feat request api

--- src/components/characterTab/characterTab.jsx
+++ src/components/characterTab/characterTab.jsx
@@ -7,25 +7,46 @@ import {
   TextInfo,
   Icon,
   Title,
+  GridContainer,
 } from "./styled";
 import bruxo from "./../../assets/bruxo.png";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { RiAddLine  } from "react-icons/ri";
+import { useState, useEffect } from "react";
+import { RiAddLine } from "react-icons/ri";
 import ModalCreateCharacter from "../modal/modalCreateCharacter/modalCreateCharacter";
+import axios from "axios";
 
 const CharacterTab = () => {
   const [openModal, setOpenModal] = useState(false);
   const navigate = useNavigate();
+  const [characterData, setCharacterData] = useState([]);
   const handleContainerClick = () => {
     navigate("/perfil/character");
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    // Verifique se o token está disponível
+    if (token) {
+      // Configure o cabeçalho de autorização
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      axios.get("http://localhost:8080/personagem", config).then((response) => {
+        setCharacterData(response.data); // Supondo que os dados estejam em um array
+      });
+    }
+  }, []);
+
   return (
     <Container>
       <Title>
         <h1>TODOS OS PERSONAGENS:</h1>
         <Icon onClick={() => setOpenModal(true)}>
-          <RiAddLine  />
+          <RiAddLine />
         </Icon>
         <h1>ADICIONAR</h1>
       </Title>
@@ -34,28 +55,32 @@ const CharacterTab = () => {
         setModalOpen={() => setOpenModal(!openModal)}
       />
 
-      <button
-        onClick={handleContainerClick}
-        style={{
-          backgroundColor: "transparent",
-          border: "none",
-          padding: 0,
-          cursor: "pointer",
-        }}
-      >
-        <BoxPersonagem>
-          <ImageRow>
-            <ImageContainer>
-              <Imagem src={bruxo} alt="Bruxo" />
-            </ImageContainer>
-            <TextInfo>
-              <p>Nome: Kira Fiore</p>
-              <p>Status: Em Campanha</p>
-              <p>Sistema: Tormenta</p>
-            </TextInfo>
-          </ImageRow>
-        </BoxPersonagem>
-      </button>
+      <GridContainer>
+        {characterData.map((character, index) => (
+          <button
+            onClick={handleContainerClick}
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              padding: 0,
+              cursor: "pointer",
+            }}
+          >
+            <BoxPersonagem>
+              <ImageRow>
+                <ImageContainer>
+                  <Imagem src={bruxo} alt="Bruxo" />
+                </ImageContainer>
+                <TextInfo>
+                  <p>Nome: {character.nome}</p>
+                  <p>Status: {character.status}</p>
+                  <p>Sistema: {character.sistemaDoRPG}</p>
+                </TextInfo>
+              </ImageRow>
+            </BoxPersonagem>
+          </button>
+        ))}
+      </GridContainer>
     </Container>
   );
 };
